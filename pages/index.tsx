f6efe0634dcfb135c181fffd7bef6b3699795f89
savisut <savisut@centralpattana.co.th>
2024-07-30 11:06:18 +0700

    Update latest version

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import Navbar from "@/components/Navbar";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
