39a53aafcfb76e2cabbcf4cb277e88999bb9f9b6
nearlys40 <nearlysza@gmail.com>
2024-08-17 16:20:49 +0700

    Add default-icon menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -57,7 +57,12 @@ const Navbar = () => {
           <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
             <BsBell />
           </div>
-          <div></div>
+          <div className="flex flex-row items-center gap-2 cursor-pointer relative">
+            <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+              <img src="/images/default-blue.png" alt="" />
+            </div>
+            <BsChevronDown className="text-white transition" />
+          </div>
         </div>
       </div>
     </nav>
